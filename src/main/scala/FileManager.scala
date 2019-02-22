1bfee1e9fe9e81eff45e665069e57ecaf8c92e11
Ziółkowski Krzysztof <ziolkowski15@gmail.com>
2019-02-22 11:13:10 +0100

    Improved getListOfFiles() method

--- src/main/scala/FileManager.scala
+++ src/main/scala/FileManager.scala
@@ -2,10 +2,15 @@ import java.io.File
 
 class FileManager {
 
-  def getListOfFiles(directory: String): List[File] = {
-    val d = new File(directory)
+  val rightExtensions = List("jpg", "png")
+
+  def getListOfFiles(dir: String):List[File] = {
+    val extensions = rightExtensions
+    val d = new File(dir)
     if (d.exists && d.isDirectory) {
-      d.listFiles.filter(_.isFile).toList
+      d.listFiles.filter(_.isFile).toList.filter { file =>
+        extensions.exists(file.getName.endsWith(_))
+      }
     } else {
       List[File]()
     }
